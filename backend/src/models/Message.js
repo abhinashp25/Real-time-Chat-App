d323ca91f3f43f2e5e7b91b2b29ffb8870c4f058
Abhinash Pradhan <abhinashpradhan8658@gmail.com>
2026-02-28 20:53:13 +0530

    message routes optimized

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -12,7 +12,7 @@ const messageSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-    text: {
+    text: {      
       type: String,
       trim: true,
       maxlength: 2000,
